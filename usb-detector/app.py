d447609fcd03e4ebc27b662f1fa22d7a54c043e7
Jens Peter Secher <jps@qdevil.com>
2022-08-10 14:45:11 +0200

    usb detector

---
+++ usb-detector/app.py
@@ -0,0 +1,35 @@
+import sys
+import serial.tools.list_ports as list_ports
+import serial
+
+device_signature = '0403:6014'
+baud_rate = 921600
+
+
+def find_serial_device():
+    candidates = list(list_ports.grep(device_signature))
+    if not candidates:
+        raise ValueError(f'No device with signature {device_signature} found')
+    if len(candidates) > 1:
+        raise ValueError(f'More than one device with signature {device_signature} found')
+    return candidates[0].device
+
+
+def detect_device():
+    serial_device = find_serial_device()
+    print(f'Found QDAC-II on {serial_device}')
+    connection = serial.Serial(serial_device, baud_rate, timeout=1)
+    raw = bytes('*idn?\n', 'utf8')
+    connection.write(raw)
+    data = connection.read(30)
+    id = data.decode('utf-8')
+    print(f'identification: {id}')
+
+
+if __name__ == '__main__':
+    try:
+        detect_device()
+        sys.exit(0)
+    except Exception as error:
+        print(f'Error: {error}')
+        sys.exit(1)
