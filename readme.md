c04bb09e1edbb86d7463f1e45b43fe1f8d0c1cba
Jens Peter Secher <jps@qdevil.com>
2023-11-14 12:28:49 +0100

    Detect both QSwitch and QDAC-II

--- readme.md
+++ readme.md
@@ -1,14 +1,31 @@
 # Tools for QDevil QDAC-II
 
-This repository contains additional tools and examples for QDAC-II.  See https://qdevil.com/w82mkw6a/ for more information and examples.
+This repository contains additional tools and examples for QDAC-II and QSwitch.  See https://qm.quantum-machines.co/qe-documentation-firmware for more information and examples.
 
 ## List of tools
 
-- `usb_detector`: Detect a QDAC-II connected through USB and display port and setup information.
+- `usb_detector`: Detect a QDAC-II or QSwitch connected through USB and display port and setup information.
 
+## First-time Setup
+
+### Linux / macOS
+
+In a Terminal:
+
+	$ scripts/setup-virtual-env.sh
+	$ scripts/install-libs.sh
+
+### Windows
+
+In PowerShell:
+
+	PS> python -m venv venv
+	PS> scripts\install-libs.bat
+	
+	
 ## How to use
 
-Most applications and examples are written in Python 3, so you will need to set up a virtual python environment, see First-time Setup below.
+Most applications and examples are written in Python 3, so you will need to set up a virtual python environment, see First-time Setup above.
 
 Each subfolder contains a separate tool or example.
 
@@ -27,20 +44,3 @@ In PowerShell:
 
 	PS> venv\Scripts\activate.ps1
 	PS> python src\usb_detector.py
-
-
-## First-time Setup
-
-### Linux / macOS
-
-In a Terminal:
-
-	$ scripts/setup-virtual-env.sh
-	$ scripts/install-libs.sh
-
-### Windows
-
-In PowerShell:
-
-	PS> python -m venv venv
-	PS> scripts\install-libs.bat
--- src/common/connection.py
+++ src/common/connection.py
@@ -1,25 +1,42 @@
 import serial.tools.list_ports as list_ports
 import serial
 import re
-
-device_signature = '0403:6014'
-baud_rate = 921600
+from dataclasses import dataclass
 
 
-def find_serial_device():
-    candidates = list(list_ports.grep(device_signature))
-    if not candidates:
-        raise ValueError(f'No device with signature {device_signature} found')
-    if len(candidates) > 1:
-        raise ValueError(f'More than one device with signature {device_signature} found')
-    return candidates[0].device
+@dataclass
+class Device:
+    kind: str
+    signature: str
+    baud_rate: int
+
+
+devices = [
+    Device('QDAC-II', '0403:6014', 921600),
+    Device('QSwitch', '04D8:00DD', 9600),
+]
+
+
+def find_serial_devices():
+    result = []
+    for device in devices:
+        candidates = list(list_ports.grep(device.signature))
+        if len(candidates) > 1:
+            raise ValueError(f'More than one device with signature {device.signature} found')
+        if len(candidates) == 1:
+            result.append((device, candidates[0].device))
+    if not result:
+        raise ValueError('No devices found')
+    return result
 
 
 def get_id(connection):
     raw = bytes('*idn?\n', 'utf8')
     connection.write(raw)
-    data = connection.read(30)
-    return data.decode('utf-8')
+    data = connection.read(40)
+    answer = data.decode('utf-8')
+    match = re.search('[^\n]+', answer)
+    return match[0]
 
 
 def get_ip_addr(connection):
@@ -36,7 +53,7 @@ def get_mac_addr(connection):
     connection.write(raw)
     data = connection.read(30)
     answer = data.decode('utf-8')
-    match = re.search('[0-9A-F]+', answer)
+    match = re.search('[0-9A-F:]+', answer)
     return match[0]
 
 
@@ -63,7 +80,7 @@ def get_name(connection):
     connection.write(raw)
     data = connection.read(30)
     answer = data.decode('utf-8')
-    match = re.search('[0-9a-f._-]+', answer)
+    match = re.search('[^"]+', answer)
     return match[0]
 
 
@@ -75,21 +92,22 @@ def get_dhcp(connection):
 
 
 def report_device_info():
-    serial_device = find_serial_device()
-    print(f'Found QDAC-II')
-    print(f'Port: {serial_device}')
-    connection = serial.Serial(serial_device, baud_rate, timeout=0.2)
-    id = get_id(connection)
-    print(f'identification: {id}')
-    mac_addr = get_mac_addr(connection)
-    print(f'MAC address: {mac_addr}')
-    gateway = get_gateway(connection)
-    print(f'Gateway: {gateway}')
-    mask = get_mask(connection)
-    print(f'Network mask: {mask}')
-    ip_addr = get_ip_addr(connection)
-    print(f'IP address: {ip_addr}')
-    name = get_name(connection)
-    print(f'Host name: {name}')
-    dhcp = 'yes' if get_dhcp(connection) else 'no'
-    print(f'DHCP enabled: {dhcp}')
+    for device, serial_info in find_serial_devices():
+        print(f'Found: {device.kind}')
+        print(f'Port: {serial_info}')
+        connection = serial.Serial(serial_info, device.baud_rate, timeout=0.2)
+        id = get_id(connection)
+        print(f'identification: {id}')
+        mac_addr = get_mac_addr(connection)
+        print(f'MAC address: {mac_addr}')
+        gateway = get_gateway(connection)
+        print(f'Gateway: {gateway}')
+        mask = get_mask(connection)
+        print(f'Network mask: {mask}')
+        ip_addr = get_ip_addr(connection)
+        print(f'IP address: {ip_addr}')
+        name = get_name(connection)
+        print(f'Host name: {name}')
+        dhcp = 'yes' if get_dhcp(connection) else 'no'
+        print(f'DHCP enabled: {dhcp}')
+        print('')
